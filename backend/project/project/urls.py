4d787b0edee941608991388278222fbc5640299e
Sam-209 <samuelsilipi2@gmail.com>
2025-09-14 16:01:08 -0300

    Update urls.py

--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import lista_clientes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('core.urls'))
+]
+urlpatterns = [
+    path('clientes/', lista_clientes, name='lista_clientes'),
 ]
