bcbaca61ae1d66d45801c3001a57de89da84a1d9
Sam-209 <samuelsilipi2@gmail.com>
2025-09-14 16:04:06 -0300

    Update urls.py

--- backend/project/core/urls.py
+++ backend/project/core/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('clientes/', ClienteListCreateView.as_view(), name='clientes-list-create'),
     path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='clientes-detail'),
 ]
+urlpatterns = [
+    path('clientes/', lista_clientes, name='lista_clientes'),
+]
