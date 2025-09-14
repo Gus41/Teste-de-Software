a9c35d4092c0c6a879ed1474e878617525d35256
Sam-209 <samuelsilipi2@gmail.com>
2025-09-14 19:26:04 -0300

    Update urls.py

--- backend/project/core/urls.py
+++ backend/project/core/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import ClienteListCreateView, ClienteDetailView
 from .views import ClienteViewSet
 
-router = DefaultRouter()
-router.register(r'clientes', ClienteViewSet)
-
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
@@ -12,6 +9,4 @@ urlpatterns = [
     path('clientes/', ClienteListCreateView.as_view(), name='clientes-list-create'),
     path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='clientes-detail'),
 ]
-urlpatterns = [
-    path('clientes/', lista_clientes, name='lista_clientes'),
-]
+
