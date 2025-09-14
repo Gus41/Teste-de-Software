ae8c4f54c324fc7c8d5fb6230da4e9e5f01b8c9b
Sam-209 <samuelsilipi2@gmail.com>
2025-09-14 19:20:26 -0300

    Update views.py

--- backend/project/core/views.py
+++ backend/project/core/views.py
@@ -2,11 +2,7 @@ from rest_framework import status, generics
 from .models import Cliente
 from .serializers import ClienteSerializer
 from django.shortcuts import render
-from .views import lista_clientes
 
-def pagina_criar_cliente(request):
-    return render(request, 'CriarCliente.html')
-    
 class ClienteListCreateView(generics.ListCreateAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
@@ -15,6 +11,3 @@ class ClienteDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
     
-def lista_clientes(request):
-    clientes = Cliente.objects.select_related('conta_corrente').all()
-    return render(request, 'Clientes.html', {'clientes': clientes})
