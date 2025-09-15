6da65a47bf06c5daed432fd72f7d4924abb5e26a
Gustavo Damasio <123191793+Gus41@users.noreply.github.com>
2025-09-15 08:42:53 -0300

    feat: separate logic

--- backend/project/core/urls.py
+++ backend/project/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ClienteListCreateView, ClienteDetailView
+from .views import ClienteListCreateView, ClienteDetailView, ContaCorrenteDetailView, ContaCorrenteListCreateView
 
 urlpatterns = [
     path('clientes/', ClienteListCreateView.as_view(), name='clientes-list-create'),
     path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='clientes-detail'),
+    path('contas/', ContaCorrenteListCreateView.as_view(), name='contas-list-create'),
+    path('contas/<int:pk>/', ContaCorrenteDetailView.as_view(), name='contas-detail')
 ]
