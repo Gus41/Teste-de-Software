c29820a4279e16f1fe324a3be305a26b0d57b6b9
Sam-209 <samuelsilipi2@gmail.com>
2025-09-20 03:39:35 -0300

    Update ApiManager_test.js

--- frontend/modules/testes/ApiManager_test.js
+++ frontend/modules/testes/ApiManager_test.js
@@ -1,1 +1,10 @@
+import { ApiManager } from './ApiManager.js';
+import { Client } from '../modules/models/Client.js';
+global.fetch = jest.fn();
+
+beforeEach(() => {
+    fetch.mockClear(); 
+});
+
+
 
