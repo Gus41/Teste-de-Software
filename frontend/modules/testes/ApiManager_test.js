587aa9f868ff34697a97b99591d517ef931de7f6
Sam-209 <samuelsilipi2@gmail.com>
2025-09-20 03:42:57 -0300

    Update ApiManager_test.js

--- frontend/modules/testes/ApiManager_test.js
+++ frontend/modules/testes/ApiManager_test.js
@@ -6,5 +6,30 @@ beforeEach(() => {
     fetch.mockClear(); 
 });
 
+test('getClients deve retornar uma lista de instâncias de Client', async () => {
+    // Dados fake simulando retorno do backend
+    const fakeClients = [
+        {
+            id: 1,
+            nome: "João",
+            idade: 30,
+            email: "joao@example.com",
+            ativo: true,
+            conta_corrente: "12345-6"
+        }
+    ];
 
+    
+    fetch.mockResolvedValueOnce({
+        json: async () => fakeClients
+    });
+
+    const api = new ApiManager('http://fakeapi.com');
+    const clients = await api.getClients();
+
+    expect(fetch).toHaveBeenCalledWith('http://fakeapi.com/clientes');
+
+    expect(clients[0]).toBeInstanceOf(Client);
+    expect(clients[0].nome).toBe("João");
+});
 
